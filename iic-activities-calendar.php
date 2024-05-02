b6a91969f1240e8238544ee923a48dc541d0884d
Ug0510 <uditgg0510@gmail.com>
2024-05-02 17:00:47 +0530

    Converting internal css to external in IIC pages

--- iic-activities-calendar.php
+++ iic-activities-calendar.php
@@ -2,129 +2,15 @@
 include 'head.php';
 ?>
 <link rel="stylesheet" href="./assets/css/custom-accordian.css">
-<style>
-    .table> :not(caption)>*>* {
-        padding: 0.5rem !important;
-        color: var(--bs-table-color-state, var(--bs-table-color-type, var(--bs-table-color)));
-        background-color: var(--bs-table-bg);
-        border-bottom-width: var(--bs-border-width);
-        box-shadow: inset 0 0 0 9999px var(--bs-table-bg-state, var(--bs-table-bg-type, var(--bs-table-accent-bg)));
-    }
-
-    .big-card {
-        width: 70vw;
-        box-shadow: 2px 2px 10px 2px rgba(0, 0, 0, 0.3);
-        height: auto;
-        margin: auto;
-        position: relative;
-        padding-bottom: 50px;
-    }
-
-    .big-card-heading {
-        width: 95%;
-        height: 4rem;
-        position: absolute;
-        top: 0;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #001055;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .big-card-heading p {
-        color: white;
-        margin-bottom: 0;
-        font-size: 28px;
-        text-transform: uppercase;
-        font-weight: 500;
-    }
-
-    .big-card .min-text {
-        color: #fe8d00;
-        font-size: 12px;
-        margin: 0;
-        padding: 0;
-        width: fit-content;
-    }
-
-    .big-card .main-text {
-        font-size: 18px;
-        text-align: justify;
-    }
-
-    .m-custom-card {
-        height: 150px;
-        box-shadow: 4px 4px 6px 1px rgba(0, 0, 0, 0.06);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        /* background: linear-gradient(115deg, rgba(251, 215, 134, 0.3), rgba(198, 255, 221, 0.3)); */
-        background-color: rgba(205, 205, 205, 0.2);
-        /* border:1px solid rgba(0, 0, 0, 0.01);  */
-        transition: 0.2s all;
-    }
-
-    .m-custom-card img {
-        width: 20%;
-    }
-
-    .m-custom-card span {
-        text-transform: uppercase;
-        font-size: 12px;
-        margin-top: 20px;
-        width: fit-content;
-        font-weight: 500;
-        text-align: center;
-    }
-
-    .m-custom-card:hover {
-        scale: 0.99;
-        box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.05);
-        cursor: pointer;
-    }
-
-    @media screen and (max-width:992px) {
-        .big-card {
-            width: 90vw;
-        }
-    }
-
-    @media screen and (max-width:480px) {
-        .big-card {
-            width: 90vw;
-        }
-
-        .big-card-heading {
-            width: 85%;
-        }
-
-        .big-card-heading p {
-            font-size: 19px;
-        }
-
-        .big-card .main-text {
-            font-size: 14px;
-        }
-
-        .m-custom-card span {
-            font-size: 15px;
-        }
-    }
-</style>
-
 
 <?php
 include 'sidebar/iic_sidebar.php';
 ?>
 
-                <div class="col-12 col-md-9 col-lg-9 mt-3 mt-sm-0 pt-5 ">
+                <div class="col-12 col-md-9 mt-3 mt-sm-0 pt-5 ">
 
                     <div class="row mx-0">
-                        <h2 class=" text-center d-block d-md-none" style="color:#001055; text-transform: capitalize; font-weight:700!important; font-size:24px">IIC Activities <span>Calendar</span></h2>
-                        <h2 class=" text-center d-none d-md-block" style="color:#001055; text-transform: capitalize; font-weight:700!important; font-size:2.4vmax">IIC Activities <span>Calendar</span></h2>
+                        <h1 class="text-center tmu-text-primary iic-page-heading fw-bold"><span> IIC Activities </span> <span>Calendar</span></h1>
 
                         <div class="container">
                             <div class="row">
