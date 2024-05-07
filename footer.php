0ce913e0be0a24e7f5e645407f1eada2d45a7f79
Ug0510 <uditgg0510@gmail.com>
2024-05-07 10:30:22 +0530

    create read more option

--- footer.php
+++ footer.php
@@ -210,6 +210,18 @@
 
 <script src="./assets/js/index.js"></script>
 
+
+<script>
+    $('.read-more').click(function() {
+        $(this).prev().slideToggle();
+        if (($(this).text()) == "Read More") {
+            $(this).text("Read Less");
+        } else {
+            $(this).text("Read More");
+        }
+    });
+</script>
+
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
 
 </div>
