0955bfb0b5b784f2331f70f206bf3140cb7cd45e
Ug0510 <uditgg0510@gmail.com>
2024-05-09 17:23:08 +0530

    Fix CSS structure in TMU  Vision & Mission Page

--- tmu_vision&mission.php
+++ tmu_vision&mission.php
@@ -2,31 +2,8 @@
 include 'head.php';
 ?>
 
-<style>
-	.color {
-		color: #FF7900 !important;
-	}
-
-	h3 {
-		margin: 0 0 5px 0;
-	}
-
-	.font-body {
-		font-size: 22px;
-		color: #001055;
-		font-weight: 700;
-	}
-
-	p {
-		margin-bottom: 15px !important;
-	}
-</style>
-
 <section id="content">
 	<div class="content-wrap py-0">
-
-
-
 		<div class="container-fluid my-0  px-0 px-md-3">
 
 
@@ -34,17 +11,19 @@ include 'head.php';
 				<?php
 				include 'sidebar/about_tmu_sidebar.php';
 				?>
-				<div class="col-12 col-md-10 mt-3 mt-sm-0 pt-5 ">
+				<div class="col-12 col-md-10 mt-3 mt-sm-0 pt-3 pt-md-5 px-4">
 
 
 					<div class="row align-items-end">
-						<div class="col-sm-12 col-md-12 text-justify">
-							<h2 class=" text-center" style="color:#001055; text-transform: capitalize; font-weight:700!important; font-size:2.4vmax">Vision <span>& Mission</span></h2>
-							<h3 class="fw-bold text-transform-none" style="font-size: 22px; letter-spacing: -1px;">About TMU University</h3>
-							<p>On National Highway 9, 144 Km from New Delhi, TMU stands committed to the ideals of Lord
+						<div class="col-sm-12 col-md-12 text-justify ">
+							<h1 class="tmu-text-primary tmu-page-heading"><span>Vision &</span><span> Mission</span></h1>
+
+
+							<h3 class="fw-bold tmu-text-primary mb-1"><span>About </span><span>TMU University</span></h3>
+							<p class="mb-3">On National Highway 9, 144 Km from New Delhi, TMU stands committed to the ideals of Lord
 								Mahaveer - Right Philosophy, Right Knowledge and Right Conduct in all spheres of
 								activity and aspires to be the ultimate destination for world-class education.</p>
-							<p>As a multi-disciplinary varsity, we offer career-oriented programmes at all levels, i.e.,
+							<p class="mb-3">As a multi-disciplinary varsity, we offer career-oriented programmes at all levels, i.e.,
 								UG, PG, and Doctoral degrees across diverse streams <a href="https://www.tmu.ac.in/medical-college-and-research-centre" target="_blank">Medical</a>, <a href="https://www.tmu.ac.in/dental-college-and-research-centre" target="_blank">Dental</a>, <a href="https://www.tmu.ac.in/college-of-pharmacy" target="_blank">Pharmacy</a>, <a href="https://www.tmu.ac.in/college-of-nursing" target="_blank">Nursing</a>, <a href="https://www.tmu.ac.in/college-of-paramedical-sciences" target="_blank">Paramedical Sciences</a>, <a href="https://www.tmu.ac.in/department-of-physiotherapy" target="_blank">Physiotherapy</a>, <a href="https://www.tmu.ac.in/tmimt-college-of-management" target="_blank">Hospital
 									Administration</a>, <a href="https://www.tmu.ac.in/tmimt-college-of-physical-education" target="_blank">Physical Education</a>, <a href="https://www.tmu.ac.in/tmimt-college-of-management" target="_blank">Management</a>, <a href="https://www.tmu.ac.in/faculty-of-engineering" target="_blank">Engineering</a>,
 								<a href="https://www.tmu.ac.in/faculty-of-engineering" target="_blank">Polytechnic</a>,
@@ -54,33 +33,31 @@ include 'head.php';
 						</div>
 					</div>
 					<!-- Write content for right side here  -->
-					<div class="heading-block border-bottom-0 text-justify mx-auto mb-0">
-						<h3 class="fw-bold text-transform-none" style="font-size: 22px; letter-spacing: -1px;">Our
-							Vision</h3>
+					<div class="text-justify mx-auto mb-0 mt">
+						<h3 class="fw-bold tmu-text-primary mt-2 mb-0"><span>Our </span><span> Vision</span></h3>
 						<p>To be recognized as a premier institution of excellence providing high quality education,
 							research and consultancy services to the society.</p>
 					</div>
 
-					<div class="heading-block border-bottom-0 text-justify mx-auto mb-0">
-						<h3 class="fw-bold text-transform-none" style="font-size: 22px; letter-spacing: -1px;">Our
-							Mission</h3>
+					<div class="text-justify mx-auto mb-0">
+						<h3 class="fw-bold tmu-text-primary mt-4 mb-0"><span>Our </span><span> Mission</span></h3>
 						<p>Our endeavour is to impart knowledge and develop critical skills necessary to succeed both in
 							professional and personal life by promoting learning supported by world-class faculty,
 							infrastructure, technology, curricula and collaborative teaching and research with premier
 							institutions in India and abroad.</p>
 					</div>
 
-					<div class="heading-block border-bottom-0 text-justify mx-auto mb-0">
-						<h3 class="fw-bold text-transform-none" style="font-size: 22px; letter-spacing: -1px;">Our Focus
-						</h3>
+					<div class="text-justify mx-auto mb-0">
+						<h3 class="fw-bold tmu-text-primary mt-4 mb-0"><span>Our  </span><span> Focus </span></h3>
+						
 						<p>In our quest to make the university an ultimate destination for world-class education,
 							following nine-fold priority areas have been identified for actions:</p>
 					</div>
-					<div class="container">
+					<div class="container mb-5">
 						<div class="row mt-2">
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Teaching and Learning</h4>
 										<p class="mb-2">Create conducive environment for an interactive and application
@@ -90,7 +67,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Research</h4>
 										<p class="mb-2">Foster research orientation in students and faculty in basic and
@@ -100,7 +77,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Consultancy Services</h4>
 										<p class="mb-2">Take academic proficiency to corporate and community by
@@ -110,7 +87,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">People</h4>
 										<p class="mb-2">Make Teerthanker Mahaveer University a preferred destination for
@@ -120,7 +97,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Global Perspective</h4>
 										<p class="mb-2">Encompass a national and international viewpoint into teaching,
@@ -130,7 +107,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Good Governance</h4>
 										<p class="mb-2">Bring transparency and accountability in university operations.
@@ -140,7 +117,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Sports and Culture</h4>
 										<p class="mb-2">Provide ample opportunities to develop sportsmanship and love
@@ -150,7 +127,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Enterpreneurial Culture</h4>
 										<p class="mb-2">Nurture entrepreneurial capabilities to accelerate growth.</p>
@@ -159,7 +136,7 @@ include 'head.php';
 							</div>
 							<div class="col-md-6 col-sm-6">
 								<div class="media d-flex">
-									<i class="h2 mb-0 lh-sm bi-check-circle color me-2"></i>
+									<i class="h2 mb-0 lh-sm bi-check-circle tmu-color me-2"></i>
 									<div class="media-body">
 										<h4 class="mb-1 font-body">Social Relevance</h4>
 										<p class="mb-2">Position itself as a catalyst in social change.</p>
@@ -175,7 +152,6 @@ include 'head.php';
 </section><!-- #content end -->
 
 </div><!-- #wrapper end -->
-<br /><br />
 
 
 <?php
