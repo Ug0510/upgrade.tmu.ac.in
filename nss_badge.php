977258b0e085c36faf0ef7c5d155e0a7c6da2b9e
Ug0510 <uditgg0510@gmail.com>
2024-05-14 11:51:20 +0530

    Redoing NSS pages

--- nss_badge.php
+++ nss_badge.php
@@ -21,14 +21,14 @@ include 'head.php';
           <!-- Write content for right side here  -->
 
           <div class="row mx-0">
-            <h1 class="tmu-text-primary tmu-page-heading">NSS Logo <span> & Badges</span></h1>
+            <h1 class="tmu-text-primary tmu-page-heading"><span>NSS Logo </span><span> & Badges</span></h1>
 
             <div class="container">
               <div class="row">
-                <h3 class="mb-2 pb-0 tmu-text-primary"><span>Logo Of</span><span> NSS</span></h3>
+                <h3 class="mb-2 pb-0 tmu-text-primary fw-bold"><span>Logo Of</span><span> NSS</span></h3>
                 <p class="mb-3">The symbol for the NSS has been based on the giant Rath Wheel of the world famous Konark Sun Temple (The Black Pagoda) situated in Orissa, India. The wheel portrays the cycle of creation, preservation and release and signifies the movement in life across time and space, the symbol thus stands for continuity as well as change and implies the continuous striving of NSS for social change.</p>
 
-                <h3 class="mb-2 pb-0 tmu-text-primary"><span>Badge of</span> <span>NSS</span></h3>
+                <h3 class="mb-2 pb-0 tmu-text-primary fw-bold"><span>Badge of</span> <span>NSS</span></h3>
                 <p>The NSS symbol is embossed on the NSS badge. The NSS volunteers wear it while undertaking any programme of community service. The Konark wheel in the symbol has eight bars which represent the 24 hours of the day. The Red colour in the badge indicates that the NSS volunteers are full of blood. i.e. lively, active energetic and full of high spirit. The Navy Blue colour indicates the cosmos of which the NSS is a tiny part, ready to contribute its share for the welfare of mankind.</p>
               </div>
             </div>
--- nss_day&song.php
+++ nss_day&song.php
@@ -26,10 +26,10 @@ include 'head.php';
             <h1 class="tmu-text-primary tmu-page-heading"><span>NSS Days </span> <span> & Songs</span></h1>
             <div class="container">
               <div class="row">
-                <h3 class="mb-2 pb-0 tmu-text-primary"><span>NSS </span> <span> Day</span></h3>
+                <h3 class="mb-2 pb-0 tmu-text-primary fw-bold"><span>NSS </span> <span> Day</span></h3>
                 <p class="mb-4">NSS was formally launched on 24th September, 1969, the birth centenary year of the Father of the Nation. Therefore, 24 September is celebrated every year as NSS Day with appropriate programmes and activities.</p>
 
-                <h3 class="mb-2 pb-0 tmu-text-primary"><span>NSS </span> <span> Song</span></h3>
+                <h3 class="mb-2 pb-0 tmu-text-primary fw-bold"><span>NSS </span> <span> Song</span></h3>
                 <p>During Silver Jubilee Year the NSS theme song has been composed. All NSS volunteers are expected to learn the theme song and sing the song during NSS programmes and celebrations. The theme song cassette is available and the theme song is given in the end of the Manual.</p>
               </div>
             </div>
