e0e7887f85ae2118964443bf1bbba0294484f08f
MathuR-A01 <mathuraayush037@gmail.com>
2024-05-15 11:16:33 +0530

    added the sidebar and added the container and headings in feedback reports

---
+++ sidebar/iqac_reports.php
@@ -0,0 +1,15 @@
+<div class="col-md-2 col-lg-2 sticky-sidebar-wrap pe-5 py-0 py-lg-3 " style="background-color: #55555510;">
+
+	<span class="undernav-locator mb-2 ms-3">
+		<span style="color:#fe8d00" ; style="font-size:24px"><a href="/iqac.php"><i class="fa-solid fa-house"></i></a></span>
+		<i class="fa-solid fa-chevron-right" style="color:#fe8d00"></i>
+		<span style="color:#001055; font-weight:700;">IQAC Reports</span>
+	</span>
+
+	<ul class="list-unstyled items-nav sticky-sidebar shop-filter mt-4 d-none d-sm-block" data-container="#shop">
+		<li class="no-divider"><a href="#" class="text-dark fw-semibold side-head" data-filter=".sf-new">Feedback Reports</a></li>
+		<li><a href="/feedback_atr_report.php" data-filter=".sf-about-iqac">Feedback Analysis & ATR</a></li>
+		<li><a href="/feedback_analysis_report.php" data-filter=".sf-quality-policy">Feedback Analysis Report</a></li>
+		<hr>
+	</ul>
+</div>
