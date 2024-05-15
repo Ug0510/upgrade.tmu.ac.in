b9bff579f20208a3f4769ebd2ef6d4d587ff108c
MathuR-A01 <mathuraayush037@gmail.com>
2024-05-15 13:24:10 +0530

    replaced the heading h1 code and added margins in code

--- mgmt_anti_ragging.php
+++ mgmt_anti_ragging.php
@@ -8,7 +8,7 @@
                 <?php
                     include 'sidebar/management_sidebar.php';
                 ?>
-                <div class="col-12 col-md-9 col-lg-9 mt-3 mt-sm-0 pt-5">
+                <div class="col-12 col-md-10 mt-3 mt-sm-0 pt-5">
                     <h1 class="tmu-text-primary tmu-page-heading"><span>Anti Ragging</span><span> Committee</span></h1>
                     <div class="container">
                         <div class="row">
--- mgmt_contactus.php
+++ mgmt_contactus.php
@@ -2,41 +2,22 @@
 include 'head.php';
 ?>
 
-<style>
-  .contact .php-email-form {
-    width: 100%;
-    height: 100%;
-    background: #fff;
-    box-shadow: 0px 2px 25px rgba(0, 0, 0, 0.1);
-    padding: 30px;
-    border-radius: 0 10px 10px 0;
-  }
-
-</style>
-
 <section id="content">
     <div class="content-wrap py-0">
-
         <div class="container-fluid my-0  px-0 px-md-3">
-
-
             <div class="row">
 
-                <?php
-            include 'sidebar/management_sidebar.php';
+            <?php
+                include 'sidebar/management_sidebar.php';
             ?>
-
                 <div class="col-12 col-md-10 col-lg-10 mt-3 mt-sm-0 pt-5 ">
                     <!-- ======= Contact Section ======= -->
                     <section id="contact" class="contact">
                         <div class="container" data-aos="fade-up">
                             <h1 class="tmu-text-primary tmu-page-heading"><span>Contact</span><span> Us</span></h1>
-
-
-
                             <div class="container">
                                 <div class="row">
-                                    <div class="col-lg-4 col-sm-6">
+                                    <div class="col-lg-4 col-sm-6 mb-3 mt-2">
                                         <h3 class="mb-2 pb-0" style="color: #001055;">The College</h3>
                                         <p>
                                             <i class="fa-solid fa-location-dot"></i> &nbsp<b>Location:</b> Opp.
@@ -51,12 +32,12 @@ include 'head.php';
                                             (+91) 591-2487244
                                         </p>
                                     </div>
-                                    <div class="col-lg-4 col-sm-6">
+                                    <div class="col-lg-4 col-sm-6 mb-3 mt-2">
                                         <h3 class="mb-2 pb-0" style="color: #001055;">Get in Touch</h3>
                                         <h4 class="pb-0 mb-0"><b></b></h4>
                                         <p>For any query related to TMIMT College of Management, you may drop message or contact the officials at college.</p>
                                     </div>
-                                    <div class="col-lg-4 col-sm-6">
+                                    <div class="col-lg-4 col-sm-6 mb-3 mt-2">
                                         <h3 class="mb-2 pb-0" style="color: #001055;">College Hours</h3>
                                         <i class="fa-solid fa-calendar-days"></i>&nbsp<b>Days :</b>Monday - Friday 9am
                                         to 5pm,
@@ -67,23 +48,22 @@ include 'head.php';
                                     <div class="col-lg-12">
                                         <form action="forms/contact.php" method="post" role="form"
                                             class="php-email-form">
-                                            <h3 class="text-left mt-1 mb-4" style="color:#001055; "> NEED MORE
-                                                INFORMATION?</h3>
+                                            <h3 class="tmu-text-primary mt-1 mb-4"><span> NEED MORE </span><span> INFORMATION?</span></h3>
                                             <div class="row">
                                                 <div class="col-md-6 form-group">
                                                     <input type="text" name="name" class="form-control" id="name"
                                                         placeholder="Your Name" required>
                                                 </div>
-                                                <div class="col-md-6 form-group mt-3 mt-md-0">
+                                                <div class="col-md-6 form-group mt-2 mt-md-0">
                                                     <input type="email" class="form-control" name="email" id="email"
                                                         placeholder="Your Email" required>
                                                 </div>
                                             </div>
-                                            <div class="form-group mt-3">
+                                            <div class="form-group mt-2">
                                                 <input type="text" class="form-control" name="contact" id="contact"
                                                     placeholder="Enter your Number" required>
                                             </div>
-                                            <div class="form-group mt-3">
+                                            <div class="form-group mt-2">
                                                 <textarea class="form-control" name="message" rows="7"
                                                     placeholder="Message" required></textarea>
                                             </div>
