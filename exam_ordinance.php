47a654c47a04d62ecb3d637a79fa4943f03cf2d1
MathuR-A01 <mathuraayush037@gmail.com>
2024-05-15 16:12:02 +0530

    correction in the examination system sidebar code & added container in the ordinance

--- exam_ordinance.php
+++ exam_ordinance.php
@@ -2,99 +2,106 @@
  include 'head.php';
 ?>
 
-<!-- Page Title
-		============================================= -->
-<?php
-include 'sidebar/examination_system_sidebar.php';
-?>
-
+<section id="content">
+    <div class="content-wrap py-0">
+        <div class="container-fluid my-0  px-0 px-md-3">
+            <div class="row">
+                <?php include 'sidebar/examination_system_sidebar.php'; ?>
                 <div class="col-12 col-md-10 mt-3 mt-sm-0 pt-5 ">
-        			<h1 class="tmu-text-primary tmu-page-heading"><span> Examination </span><span> Ordinance</span></h1>
-          					<div class="row">
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto Academic Session 2012-13</h5>
-										
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/pdf/policy/Examination_Ordinance.pdf" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
+                    <h1 class="tmu-text-primary tmu-page-heading"><span> Examination </span><span> Ordinance</span></h1>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto
+                                            Academic Session 2012-13</h5>
+                                        <a href="https://www.tmu.ac.in/pdf/policy/Examination_Ordinance.pdf"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto Academic Session 2012-13</h5>
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/tmu/exam-ordinance#!" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto
+                                            Academic Session 2012-13</h5>
+                                        <a href="https://www.tmu.ac.in/tmu/exam-ordinance#!"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">From Academic session 2013-14</h5>
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/academic_calendars/Academic-Calendar-2020-21-First-Year_compressed.pdf" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">From
+                                            Academic session 2013-14</h5>
+                                        <a href="https://www.tmu.ac.in/academic_calendars/Academic-Calendar-2020-21-First-Year_compressed.pdf"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto Academic Session 2016-17</h5>
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/academic_calendars/Academic-Calendar-2020-21-Except_First-Year_compressed.pdf" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">Upto
+                                            Academic Session 2016-17</h5>
+                                        <a href="https://www.tmu.ac.in/academic_calendars/Academic-Calendar-2020-21-Except_First-Year_compressed.pdf"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">With effect from Academic Session 2016-17</h5>
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/academic_calendars/University-Academic-Calendar-2019-20.pdf" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">With effect
+                                            from Academic Session 2016-17</h5>
+                                        <a href="https://www.tmu.ac.in/academic_calendars/University-Academic-Calendar-2019-20.pdf"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-								<div class="col-md-3 mb-3">
-      								<div class="card text-center">
-        								<div class="card-body">
-          								<!-- Icon -->
-          									<i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
-          									<!-- Document Name -->
-          									<h5 class="card-title" style="font-family:'Poppins';font-size:14px;">With effect from Academic Session 2019-20</h5>
-          									<!-- View PDF Button -->
-          								<a href="https://www.tmu.ac.in/academic_calendars/University-Academic-Calendar-2018-19.pdf" class="more-link fst-normal ms-2" style="font-family:'Poppins';font-size:12px;">View pdf <i class="bi-arrow-right"></i></a>
-        								</div>
-      								</div>
-    							</div>
-        					</div>
-      					</div>
-    				</div>
-				</section><!-- #content end -->
+                            <div class="col-md-3 mb-3">
+                                <div class="card text-center">
+                                    <div class="card-body">
+                                        <i class="fas fa-file-pdf fa-4x mb-3" style="color:#FF7900; "></i>
+                                        <h5 class="card-title" style="font-family:'Poppins';font-size:14px;">With effect
+                                            from Academic Session 2019-20</h5>
+                                        <a href="https://www.tmu.ac.in/academic_calendars/University-Academic-Calendar-2018-19.pdf"
+                                            class="more-link fst-normal ms-2"
+                                            style="font-family:'Poppins';font-size:12px;">View pdf <i
+                                                class="bi-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>        
+</section><!-- #content end -->
 
 <?php
  include 'footer.php';    
  ?>
-
