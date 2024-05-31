cf93b2661bc4c19fb6a87ef92086eabfcc4b4dc0
Ug0510 <uditgg0510@gmail.com>
2024-05-31 15:45:20 +0530

    fixing medical sidebar

--- sidebar/medical_sidebar.php
+++ sidebar/medical_sidebar.php
@@ -1,10 +1,18 @@
 
+<div class="col-12 d-sm-none" style="background-color: #55555510;">
+<span class="undernav-locator mb-2 ms-3 ">
+		<span style="color:#fe8d00" ; style="font-size:24px"><a href="/"><i class="fa-solid fa-house"></i></a></span>
+		<i class="fa-solid fa-chevron-right" style="color:#fe8d00"></i>
+		<span style="color:#001055; font-weight:700;">TMMC&RC at Glance</span>
+	</span>
+</div>
+
 <div class="col-sm-3 col-lg-2 sticky-sidebar-wrap px-2 pe-sm-5 py-0 py-lg-3 order-last order-sm-first mt-5 mt-sm-0 " style="background-color: #55555510;">
 
-<span class="undernav-locator mb-2 ms-3">
+<span class="undernav-locator mb-2 ms-3 d-none d-sm-block">
 	<span style="color:#fe8d00" ; style="font-size:20px"><a href="/"><i class="fa-solid fa-house"></i></a></span>
 	<i class="fa-solid fa-chevron-right" style="color:#fe8d00"></i>
-	<span class="fs-12 tmu-color-blue fw-bold">TMMC&RC at Glance</span>
+	<span class="tmu-color-blue fw-bold">TMMC&RC at Glance</span>
 </span>
 
 <p class="text-center fs-24 tmu-color mt-3 fw-bold tmu-text-primary d-sm-none">
